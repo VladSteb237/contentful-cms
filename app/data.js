62a38ec8fe80ecd08cb5f3497a2b349bea121cc5
VladSteb237 <vladsteb@mail.ru>
2025-05-06 11:56:47 +0300

    add new item

--- app/data.js
+++ app/data.js
@@ -7,16 +7,21 @@ const projects = [
   {
     title: "tours",
     url: "https://react-vite-projects-2-tours.netlify.app/",
-    image: "./tours.png",
+    image: "../public/tours.png",
   },
   {
     title: "reviews",
     url: "https://react-vite-projects-3-reviews.netlify.app/",
-    image: "./reviews.png",
+    image: "../public/reviews.png",
   },
   {
     title: "questions",
     url: "https://react-vite-projects-4-accordion.netlify.app/",
-    image: "./questions.png",
+    image: "../public/questions.png",
+  },
+  {
+    title: "store draft",
+    url: "https://nextjs-store-draft-one.vercel.app",
+    image: "../public/store-draft.png",
   },
 ];
